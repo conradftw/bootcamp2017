67c03ab3824f6071013077aac4b1ae705c303072
Conrad Cheung <conradftw@hotmail.ca>
2017-09-09 11:54:13 -0700

    Work this time

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+
+public class Runner {
+	public static void main(String[] args) {
+		Triangle t = new Triangle(10.3,5.5,3.2,100.2,89.7,65.3);
+		//Triangle t = new Triangle(1,1,2,2,3,3);
+
+		System.out.println("The area of our triangle is: " + t.area());
+	}
+}
---
+++ src/main/java/Triangle.java
@@ -0,0 +1,21 @@
+
+public class Triangle {
+	private double x1, y1, x2, y2, x3, y3;
+	
+	public Triangle(double x1, double y1,double x2,double y2,double x3,double y3) {
+		//notice that the triangle from line 3 has six points
+		//constructor takes input and stores those six inputs into respective doubles
+		this.x1=x1;this.y1=y1;
+		this.x2=x2;this.y2=y2;
+		this.x3=x3;this.y3=y3;
+		assert(this.area()>0.0000000001); //scream if not a triangle 
+	}
+	
+	public double area() {
+		double area = ((x1-x3)*(y2-y1)-(x1-x2)*(y3-y1))/2;
+		if (area<0)
+			area = -area;
+		return area;
+	}
+	
+}
